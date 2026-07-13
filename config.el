22d32cb604d016d14295b1e4476ac3b11b10cbb3
A2Zkiller <zureikatabdullah@gmail.com>
2026-07-13 00:31:24 -0400

    Updated Emacs Config

--- config.el
+++ config.el
@@ -49,106 +49,6 @@
   :config
   (load-theme 'catppuccin :no-confirm))
 
-(use-package evil
-  :init
-  (setq evil-want-keybinding nil)
-  (setq evil-vsplit-window-right t)
-  (setq evil-split-window-below t)
-
-  :config
-  (evil-mode 1)
-  (evil-set-undo-system 'undo-redo))
-
-(use-package evil-collection
-  :after evil
-  :config
-  (setq evil-collection-mode-list '(dashboard dired ibuffer))
-  (evil-collection-init))
-
-(use-package evil-tutor)
-
-(use-package general
-  :config
-  (general-evil-setup)
-
-  ;; Setup SPC as global leader key
-  (general-create-definer a2z/leader-key
-    :states '(normal insert visual emacs)
-    :keymaps 'override
-    :prefix "SPC"
-    :global-prefix "M-SPC")
-
-  (a2z/leader-key
-    "SPC" '(counsel-M-x :wk "Counsel M-x")
-    "." '(find-file :wk "Find file")
-    "f r" '(counsel-recentf :wk "Find recent files")
-    "f c" '((lambda () (interactive) (find-file (expand-file-name "config.org" user-emacs-directory))) :wk "Edit Config")
-    "r c" '((lambda () (interactive) (load-file user-init-file)) :wk "Reload Config")
-    "c l" '(comment-line :wk "Comment line"))
-
-  (a2z/leader-key
-    "b" '(:ignore t :wk "Buffer")
-    "b b" '(switch-to-buffer :wk "Switch buffer")
-    "b k" '(kill-current-buffer :wk "Kill this buffer")
-    "b n" '(next-buffer :wk "Next buffer")
-    "b p" '(previous-buffer :wk "Previous buffer")
-    "b r" '(revert-buffer :wk "Reload buffer"))
-
-  (a2z/leader-key
-    "e" '(:ignore t :wk "Evaluate")
-    "e b" '(eval-buffer :wk "Evaluate Buffer")
-    "e d" '(eval-defun :wk "Evaluate Defun")
-    "e e" '(eval-expression :wk "Evaluate Expression")
-    "e r" '(eval-region :wk "Evaluate Region"))
-
-  (a2z/leader-key
-    "h" '(:ignore t :wk "Help")
-    "h f" '(describe-function :wk "Describe Function")
-    "h v" '(describe-variable :wk "Describe Variable"))
-
-  (a2z/leader-key
-    "m" '(:ignore t :wk "Org")
-    "m a" '(org-agenda :wk "Org agenda")
-    "m e" '(org-export-dispatch :wk "Org export dispatch")
-    "m i" '(org-toggle-item :wk "Org toggle item")
-    "m t" '(org-todo :wk "Org todo")
-    "m B" '(org-babel-tangle :wk "Org babel tangle")
-    "m T" '(org-todo-list :wk "Org todo list"))
-  
-  (a2z/leader-key
-    "m b" '(:ignore t :wk "Tables")
-    "m b -" '(org-table-insert-hline :wk "Insert hline in table"))
-  
-  (a2z/leader-key
-    "m d" '(:ignore t :wk "Date/deadline")
-    "m d t" '(org-time-stamp :wk "Org time stamp"))
-
-  (a2z/leader-key
-    "t" '(:ignore t :wk "Toggle")
-    "t l" '(display-line-numbers-mode :wk "Toggle line numbers")
-    "t t" '(visual-line-mode :wk "Toggle visual line mode")
-    "t v" '(vterm-toggle :wk "Toggle vterm"))
-  
-  (a2z/leader-key
-    "w" '(:ignore t :wk "Windows")
-    ;; Window splits
-    "w c" '(evil-window-delete :wk "Close window")
-    "w n" '(evil-window-new :wk "New window")
-    "w s" '(evil-window-split :wk "Horizontal split window")
-    "w v" '(evil-window-vsplit :wk "Vertical split window")
-    ;; Window motions
-    "w h" '(evil-window-left :wk "Window left")
-    "w j" '(evil-window-down :wk "Window down")
-    "w k" '(evil-window-up :wk "Window up")
-    "w l" '(evil-window-right :wk "Window right")
-    "w w" '(evil-window-next :wk "Goto next window")
-    ;; Move Windows
-    "w H" '(buf-move-left :wk "Buffer move left")
-    "w J" '(buf-move-down :wk "Buffer move down")
-    "w K" '(buf-move-up :wk "Buffer move up")
-    "w L" '(buf-move-right :wk "Buffer move right"))
-)
-
 (use-package all-the-icons
   :if (display-graphic-p))
 
@@ -156,6 +56,18 @@
   :hook (dired-mode . (lambda () (all-the-icons-dired-mode t))))
 
 (use-package buffer-move)
+
+;; Move backups (filename.ext~) to separate directory
+(setq backup-directory-alist '((".*" . "~/.local/share/Trash/files/")))
+
+(use-package company
+  :diminish
+  :hook (elpaca-after-init . global-company-mode))
+      
+(use-package company-box
+  :after company
+  :diminish
+  :hook (company-mode . company-box-mode))
 
 (use-package dashboard
   :ensure t 
@@ -180,6 +92,57 @@
 
 (use-package diminish)
 
+(use-package dired-open 
+  :config
+  (setq dired-open-extensions '(("gif" . "feh")
+				("jpg" . "feh")
+				("png" . "feh")
+				("mp4" . "mpv")
+				("mkv" . "mpv"))))
+
+(use-package dired-preview
+  :after dired
+  :config 
+  (setq dired-preview-delay 0.1)
+  (dired-preview-global-mode 1))
+
+(use-package envrc
+  :hook (elpaca-after-init . envrc-global-mode))
+
+(setq visible-bell 1)
+
+(use-package evil
+  :init
+  (setq evil-want-keybinding nil)
+  (setq evil-vsplit-window-right t)
+  (setq evil-split-window-below t)
+
+  :config
+  (evil-mode 1)
+  (evil-set-undo-system 'undo-redo))
+
+(use-package evil-collection
+  :after evil
+  :config
+  ;;(setq evil-collection-mode-list '(dashboard dired ibuffer))
+  (evil-collection-init))
+
+(use-package evil-tutor)
+
+(use-package evil-org
+  :after org evil
+  :hook (org-mode . (lambda () evil-org-mode))
+  :config
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
+
+;; (with-eval-after-load 'evil-maps
+;;   (define-key evil-motion-state-map (kbd "SPC") nil)
+;;   (define-key evil-motion-state-map (kbd "RET") nil)
+;;   (define-key evil-motion-state-map (kbd "TAB") nil))
+
+;; (setq org-return-follow-links t)
+
 (set-face-attribute 'default nil
   :font "JetBrainsMono Nerd Font"
   :height 140
@@ -203,6 +166,133 @@
 ;; Emacs Client
 (add-to-list 'default-frame-alist '(font . "JetBrainsMono Nerd Font-14"))
 
+(use-package general
+  :config
+  (general-evil-setup)
+
+  ;; Setup SPC as global leader key
+  (general-create-definer a2z/leader-key
+    :states '(normal insert visual emacs)
+    :keymaps 'override
+    :prefix "SPC"
+    :global-prefix "M-SPC")
+
+  (a2z/leader-key
+    "SPC" '(counsel-M-x :wk "Counsel M-x")
+    "." '(find-file :wk "Find file")
+    "f r" '(counsel-recentf :wk "Find recent files")
+    "f c" '((lambda () (interactive) (find-file (expand-file-name "config.org" user-emacs-directory))) :wk "Edit Config")
+    "r c" '((lambda () (interactive) (load-file user-init-file)) :wk "Reload Config")
+    "f d" '(dashboard-open :wk "Open dashboard")
+    "c l" '(comment-line :wk "Comment line"))
+
+  (a2z/leader-key
+    "b" '(:ignore t :wk "Bookmarks/Buffers")
+    "b b" '(switch-to-buffer :wk "Switch to buffer")
+    "b c" '(clone-indirect-buffer :wk "Create indirect buffer copy in a split")
+    "b C" '(clone-indirect-buffer-other-window :wk "Clone indirect buffer in new window")
+    "b d" '(bookmark-delete :wk "Delete bookmark")
+    "b i" '(ibuffer :wk "Ibuffer")
+    "b k" '(kill-current-buffer :wk "Kill current buffer")
+    "b K" '(kill-some-buffers :wk "Kill multiple buffers")
+    "b l" '(list-bookmarks :wk "List bookmarks")
+    "b m" '(bookmark-set :wk "Set bookmark")
+    "b n" '(next-buffer :wk "Next buffer")
+    "b p" '(previous-buffer :wk "Previous buffer")
+    "b r" '(revert-buffer :wk "Reload buffer")
+    "b R" '(rename-buffer :wk "Rename buffer")
+    "b s" '(basic-save-buffer :wk "Save buffer")
+    "b S" '(save-some-buffers :wk "Save multiple buffers")
+    "b w" '(bookmark-save :wk "Save current bookmarks to bookmark file"))
+  
+  (a2z/leader-key
+    "d" '(:ignore t :wk "Dired")
+    "d d" '(dired :wk "Open Dired")
+    "d j" '(dired-jump :wk "Jump to current buffer dired")
+    "d n" '(neotree-dir :wk "Open directory in neotree")
+    "d p" '(dired-preview-mode :wk "Toggle dired preview"))
+
+  (a2z/leader-key
+    "e" '(:ignore t :wk "Evaluate")
+    "e b" '(eval-buffer :wk "Evaluate Buffer")
+    "e d" '(eval-defun :wk "Evaluate Defun")
+    "e e" '(eval-expression :wk "Evaluate Expression")
+    "e r" '(eval-region :wk "Evaluate Region"))
+
+  (a2z/leader-key
+    "h" '(:ignore t :wk "Help")
+    "h a" '(counsel-apropos :wk "Apropos")
+    "h b" '(describe-bindings :wk "Describe bindings")
+    "h c" '(describe-char :wk "Describe character under cursor")
+    "h d" '(:ignore t :wk "Emacs documentation")
+    "h d a" '(about-emacs :wk "About Emacs")
+    "h d d" '(view-emacs-debugging :wk "View Emacs debugging")
+    "h d f" '(view-emacs-FAQ :wk "View Emacs FAQ")
+    "h d m" '(info-emacs-manual :wk "The Emacs manual")
+    "h d n" '(view-emacs-news :wk "View Emacs news")
+    "h d o" '(describe-distribution :wk "How to obtain Emacs")
+    "h d p" '(view-emacs-problems :wk "View Emacs problems")
+    "h d t" '(view-emacs-todo :wk "View Emacs todo")
+    "h d w" '(describe-no-warranty :wk "Describe no warranty")
+    "h e" '(view-echo-area-messages :wk "View echo area messages")
+    "h f" '(describe-function :wk "Describe function")
+    "h F" '(describe-face :wk "Describe face")
+    "h g" '(describe-gnu-project :wk "Describe GNU Project")
+    "h i" '(info :wk "Info")
+    "h I" '(describe-input-method :wk "Describe input method")
+    "h k" '(describe-key :wk "Describe key")
+    "h l" '(view-lossage :wk "Display recent keystrokes and the commands run")
+    "h L" '(describe-language-environment :wk "Describe language environment")
+    "h m" '(describe-mode :wk "Describe mode")
+    "h t" '(load-theme :wk "Load theme")
+    "h v" '(describe-variable :wk "Describe variable")
+    "h w" '(where-is :wk "Prints keybinding for command if set")
+    "h x" '(describe-command :wk "Display full documentation for command"))
+
+  (a2z/leader-key
+    "m" '(:ignore t :wk "Org")
+    "m a" '(org-agenda :wk "Org agenda")
+    "m e" '(org-export-dispatch :wk "Org export dispatch")
+    "m i" '(org-toggle-item :wk "Org toggle item")
+    "m t" '(org-todo :wk "Org todo")
+    "m B" '(org-babel-tangle :wk "Org babel tangle")
+    "m T" '(org-todo-list :wk "Org todo list"))
+  
+  (a2z/leader-key
+    "m b" '(:ignore t :wk "Tables")
+    "m b -" '(org-table-insert-hline :wk "Insert hline in table"))
+  
+  (a2z/leader-key
+    "m d" '(:ignore t :wk "Date/deadline")
+    "m d t" '(org-time-stamp :wk "Org time stamp"))
+
+  (a2z/leader-key
+    "t" '(:ignore t :wk "Toggle")
+    "t l" '(display-line-numbers-mode :wk "Toggle line numbers")
+    "t t" '(visual-line-mode :wk "Toggle visual line mode")
+    "t v" '(vterm-toggle :wk "Toggle vterm")
+    "t n" '(neotree-toggle :wk "Toggle neotree"))
+  
+  (a2z/leader-key
+    "w" '(:ignore t :wk "Windows")
+    ;; Window splits
+    "w c" '(evil-window-delete :wk "Close window")
+    "w n" '(evil-window-new :wk "New window")
+    "w s" '(evil-window-split :wk "Horizontal split window")
+    "w v" '(evil-window-vsplit :wk "Vertical split window")
+    ;; Window motions
+    "w h" '(evil-window-left :wk "Window left")
+    "w j" '(evil-window-down :wk "Window down")
+    "w k" '(evil-window-up :wk "Window up")
+    "w l" '(evil-window-right :wk "Window right")
+    "w w" '(evil-window-next :wk "Goto next window")
+    ;; Move Windows
+    "w H" '(buf-move-left :wk "Buffer move left")
+    "w J" '(buf-move-down :wk "Buffer move down")
+    "w K" '(buf-move-up :wk "Buffer move up")
+    "w L" '(buf-move-right :wk "Buffer move right"))
+)
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -210,6 +300,59 @@
 (global-display-line-numbers-mode 1)
 (global-visual-line-mode t)
 (setq display-line-numbers-type 'relative)
+
+(use-package counsel
+  :after ivy
+  :config
+  (counsel-mode))
+
+(use-package ivy
+  :bind
+  (("C-c C-r" . ivy-resume)
+   ("C-x B" . ivy-switch-buffer-other-window))
+  :custom
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) ")
+  (setq enable-recursive-minibuffers t)
+  :config
+  (ivy-mode))
+
+(use-package all-the-icons-ivy-rich
+  :init
+  (all-the-icons-ivy-rich-mode 1))
+
+(use-package ivy-rich
+  :after ivy
+  :init (ivy-rich-mode 1)
+  :custom
+  (ivy-virtual-abbreviate 'full
+   ivy-rich-switch-buffer-align-virtual-buffer t
+   ivy-rich-path-style 'abbrev)
+  :config
+  (ivy-set-display-transformer 'ivy-switch-buffer 'ivy-switch-buffer-transformer))
+
+(use-package rust-mode)
+(add-hook 'rust-mode-hook #'eglot-ensure)
+
+(use-package nix-mode
+  :mode "\\.nix\\'")
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :config
+  (setq doom-modeline-height 30
+	doom-modeline-bar-width 5
+	doom-modeline-persp-name t
+	doom-modeline-persp-icon t))
+
+(use-package neotree
+  :config
+  (setq neo-smart-open t
+	neo-show-hidden-files t
+	neo-window-width 40
+	neo-window-fixed-size nil
+	inhibit-impacting-font-caches t
+	projectile-switch-project-action 'neotree-projectile-action))
 
 (use-package toc-org
   :commands toc-org-enable
@@ -230,8 +373,22 @@
   :config
   (projectile-mode 1))
 
+(use-package rainbow-delimiters
+  :hook ((emacs-lisp-mode . rainbow-delimiters-mode)))
+
 (use-package rainbow-mode
   :hook org-mode prog-mode)
+
+(electric-pair-mode 1)
+
+;; Do not pair < with > when in org mode (for org-tempo)
+(add-hook 'org-mode-hook (lambda ()
+         (setq-local electric-pair-inhibit-predicate
+                 `(lambda (c)
+                (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c))))))
+
+(global-auto-revert-mode 1)
+(delete-selection-mode 1)
 
 (use-package sudo-edit
   :config
@@ -258,3 +415,27 @@
               ;;(dedicated . t) ;dedicated is supported in emacs27
               (reusable-frames . visible)
               (window-height . 0.3))))
+
+;; required dependency
+(use-package transient :ensure t)
+
+(use-package magit)
+
+(use-package majutsu
+  :vc (:url "https://github.com/0WD0/majutsu")
+  :after magit)
+
+(which-key-mode 1)
+
+(setq which-key-side-window-location 'bottom
+	  which-key-sort-order #'which-key-key-order-alpha
+	  which-key-sort-uppercase-first nil
+	  which-key-add-column-padding 1
+	  which-key-max-display-columns nil
+	  which-key-min-display-lines 6
+	  which-key-side-window-slot -10
+	  which-key-side-window-max-height 0.25
+	  which-key-idle-delay 0.8
+	  which-key-max-description-length 25
+	  which-key-allow-imprecise-window-fit t
+	  which-key-separator " → " )
